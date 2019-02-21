697580ad174a8b831800b57ffadac45413ca7d2d
test <2799342155@qq.com>
2019-02-21 23:48:18 +0800

    git_test02

--- testIDEA01/TestDebug.java
+++ testIDEA01/TestDebug.java
@@ -3,6 +3,7 @@ package testIDEA01;
 public class TestDebug {
 
     public static void main(String[] args) {
+
         System.out.println("test");
         int[] temp={1,2,3,4,5};
         for(int i=0;i<temp.length;i++){
